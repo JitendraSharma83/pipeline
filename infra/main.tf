157680abbae5515f5206fddd9c992e06359430ef
Jitendra Kumar <jitendra@example.com>
2025-07-24 20:49:04 +0530

    add new

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.37.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "f5c092a3-a10b-4952-9791-a66c5de6792b"
+}
+
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-testpipe"
+  location = "West Europe"
+}
+
+resource "azurerm_storage_account" "sg" {
+  name                     = "pipestorage"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
